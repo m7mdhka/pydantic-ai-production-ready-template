e1f04662d88f4cf40c2c5b65eb30045069597c8c
Mohamemd Abbadi <mhamedrhamnah@gmail.com>
2025-11-29 04:03:55 +0300

    style: fixes

--- alembic/env.py
+++ alembic/env.py
@@ -1,7 +1,5 @@
 import asyncio
-
 from sqlalchemy.ext.asyncio import create_async_engine
-
 import src.models
 from alembic import context
 from src.core.config import settings
@@ -9,6 +7,15 @@ from src.database.database import Base
 
 target_metadata = Base.metadata
 
+def include_object(object, name, type_, reflected, compare_to):
+    """
+    Helps Alembic decide which database objects to track.
+    Returns False if the object should be ignored.
+    """
+    if type_ == "table" and name and name.startswith("LiteLLM"):
+        return False
+
+    return True
 
 def do_run_migrations(connection):
     context.configure(
@@ -18,6 +25,7 @@ def do_run_migrations(connection):
         target_metadata=target_metadata,
         include_schemas=True,
         version_table_schema=target_metadata.schema,
+        include_object=include_object
     )
 
     with context.begin_transaction():
@@ -29,7 +37,6 @@ async def run_migrations_online():
 
     In this scenario we need to create an Engine
     and associate a connection with the context.
-
     """
     connectable = create_async_engine(
         settings.database_url.unicode_string(),
